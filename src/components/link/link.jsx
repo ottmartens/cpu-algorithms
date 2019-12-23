7c547839fd9a219f21332e1b54fdf2362ee4a9e7
Ott Martens <ott.martens@pipedrive.com>
2019-12-23 15:05:00 +0200

    Update links

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import styles from './link.module.scss';
 
 const Link = () => (
-  <a
-    className={styles.link}
-    href="https://ottmartens.github.io/ram-algorithms/"
-  >
-    RAM Scheduler
-  </a>
+  <div className={styles.link}>
+    <a href="https://ottmartens.github.io/ram-algorithms/">RAM Scheduler</a>
+    <a href="https://ottmartens.github.io/disk-algorithms/">Disk Scheduler</a>
+  </div>
 );
 
 export default Link;
