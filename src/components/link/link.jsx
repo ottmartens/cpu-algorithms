a6bcd96fb14760d87770fa44ec528085d104119b
Ott Martens <ott.martens@pipedrive.com>
2020-01-06 01:56:59 +0200

    File allication link

--- src/components/link/link.jsx
+++ src/components/link/link.jsx
@@ -6,6 +6,7 @@ const Link = () => (
   <div className={styles.link}>
     <a href="https://ottmartens.github.io/ram-algorithms/">RAM Scheduler</a>
     <a href="https://ottmartens.github.io/disk-algorithms/">Disk Scheduler</a>
+    <a href="https://ottmartens.github.io/file-allocation/">File allocation</a>
   </div>
 );
 
